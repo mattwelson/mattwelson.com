bafa21618ce64aab9e4c4486865c6f7963102376
mattwelson <matthew.welson@gmail.com>
2018-04-07 14:38:21 +1200

    Added tags

--- gatsby-config.js
+++ gatsby-config.js
@@ -35,6 +35,15 @@ module.exports = {
       options: {
         modulePath: `${__dirname}/src/cms/cms.js`
       }
+    },
+    {
+      resolve: `gatsby-plugin-google-tagmanager`,
+      options: {
+        id: 'GTM-WJTDDHC',
+        // Include GTM in development.
+        // Defaults to false meaning GTM will only be loaded in production.
+        includeInDevelopment: false
+      }
     }
   ]
 }
