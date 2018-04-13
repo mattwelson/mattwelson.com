30256fa9beff1cbd69d373082532560245f19576
Matt Welson <matthew.welson@gmail.com>
2018-04-13 21:51:10 +1200

    Fixed the date widget

--- src/cms/preview-templates/BlogPostPreview.js
+++ src/cms/preview-templates/BlogPostPreview.js
@@ -8,7 +8,7 @@ const BlogPostPreview = ({ entry, widgetFor }) => (
     description={entry.getIn(['data', 'description'])}
     tags={entry.getIn(['data', 'tags'])}
     title={entry.getIn(['data', 'title'])}
-    date={entry.getIn(['data', 'date'])}
+    date={widgetFor('date')}
   />
 )
 
